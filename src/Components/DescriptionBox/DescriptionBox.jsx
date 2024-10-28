d8272494e109f578733300986f0ec6ac5c94a89d
Shar3113 <sharathkumar3113@gmail.com>
2024-10-28 14:58:43 +0530

    final-commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox =() =>{
+    return(
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews(132)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+Explore the latest fashion trends at SHOPPER, your go-to destination for online clothing shopping. From casual staples to chic statement pieces, find everything you need to elevate your wardrobe. Enjoy hassle-free browsing, easy checkout, and fast delivery, all backed by top-notch customer service. Shop now and discover your perfect style with Shopper.</p>
+            
+            </div>
+        </div>
+    )
+}
+export default DescriptionBox
